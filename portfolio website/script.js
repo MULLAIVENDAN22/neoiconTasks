e4c04107f2f8b57f69c65cd052cbf1a80951ef61
MULLAIVENDAN22 <kmullai2223@gmail.com>
2025-08-06 14:39:34 +0530

    my portfolio website changes 5

--- portfolio website/script.js
+++ portfolio website/script.js
@@ -1,18 +1,74 @@
-// var typed = new Typed("#typewriter", {
-//   strings: [
-//     "Frontend Developer...",
-//     "Full Stack Developer...",
-//     "Software Engineer...",
-//     "Java Developer...",
-//   ],
-//   typeSpeed: 100,
-//   backSpeed: 50,
-//   loop: true,
-// });
+var typed = new Typed("#typewriter", {
+  strings: ["Software Developer", "Full Stack Developer", "Frontend Developer"],
+  typeSpeed: 60,
+  backSpeed: 40,
+  loop: true,
+});
+const about = document.querySelector(".about");
+const profileMain = document.querySelector(".profileMain");
 
- var typed = new Typed("#typewriter", {
-      strings: ["Software Developer", "Full Stack Developer", "Frontend Developer"],
-      typeSpeed: 60,
-      backSpeed: 40,
-      loop: true
-    });
+if (window.matchMedia("(min-width: 10px) and (max-width: 575.9px)").matches) {
+  console.log("small device");
+
+  about.classList.add("textAlign");
+  //   if (profileMain.classList.contains("px-5")) {
+  //     profileMain.classList.remove("px-5");
+  //   }
+  window.addEventListener("scroll", () => {
+    const scrollTop = window.scrollY;
+    const viewportHeight = window.innerHeight;
+    const fullHeight = document.documentElement.scrollHeight;
+
+    if (scrollTop + viewportHeight >= fullHeight) {
+      setTimeout(() => {
+        const numArray = [1, 3, 4, 300];
+        const numbers = document.querySelectorAll(".numbers");
+        let index = 0;
+        timeinterval(index, numArray[index++], 1000);
+        timeinterval(index, numArray[index++], 700);
+        timeinterval(index, numArray[index++], 600);
+        timeinterval(index, numArray[index++], 20);
+
+        function timeinterval(index, text, duration) {
+          let count = 0;
+          const timer = setInterval(() => {
+            if (count <= text) {
+              numbers[index].innerHTML = `${count}`;
+              count++;
+            } else {
+              clearInterval(timer);
+              return;
+            }
+          }, duration);
+        }
+      }, 2000);
+    }
+  });
+} else {
+  if (about.classList.contains("textAlign")) {
+    about.classList.remove("textAlign");
+  }
+}
+
+setTimeout(() => {
+  const numArray = [1, 3, 4, 300];
+  const numbers = document.querySelectorAll(".numbers");
+  let index = 0;
+  timeinterval(index, numArray[index++], 1000);
+  timeinterval(index, numArray[index++], 700);
+  timeinterval(index, numArray[index++], 600);
+  timeinterval(index, numArray[index++], 20);
+
+  function timeinterval(index, text, duration) {
+    let count = 0;
+    const timer = setInterval(() => {
+      if (count <= text) {
+        numbers[index].innerHTML = `${count}`;
+        count++;
+      } else {
+        clearInterval(timer);
+        return;
+      }
+    }, duration);
+  }
+}, 2000);
