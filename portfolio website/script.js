f9fb36fa4f8ee2c9ab88a925758d2f7aac846521
MULLAIVENDAN22 <kmullai2223@gmail.com>
2025-08-06 13:17:30 +0530

    my portfolio website changes 4

---
+++ portfolio website/script.js
@@ -0,0 +1,18 @@
+// var typed = new Typed("#typewriter", {
+//   strings: [
+//     "Frontend Developer...",
+//     "Full Stack Developer...",
+//     "Software Engineer...",
+//     "Java Developer...",
+//   ],
+//   typeSpeed: 100,
+//   backSpeed: 50,
+//   loop: true,
+// });
+
+ var typed = new Typed("#typewriter", {
+      strings: ["Software Developer", "Full Stack Developer", "Frontend Developer"],
+      typeSpeed: 60,
+      backSpeed: 40,
+      loop: true
+    });
